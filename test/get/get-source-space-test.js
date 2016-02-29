1ec31837497c04626996e6faefac53657f345d1b
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-02-29 13:54:10 +0100

    test: Fixing source space tests
    Ensuring test setup is properly reset on each test, and fixing an issue with a test that depended on previous setup

--- test/get/get-source-space-test.js
+++ test/get/get-source-space-test.js
@@ -4,54 +4,61 @@ import Promise from 'bluebird'
 
 import getSourceSpace from '../../lib/get/get-source-space'
 
-const fsMock = {
-  readFileAsync: sinon.stub()
-}
-
 function setup () {
+  const fsMock = {
+    readFileAsync: sinon.stub()
+  }
+
+  const deliveryClientMock = {
+    sync: sinon.stub().returns(Promise.resolve({
+      items: [
+        {sys: {type: 'Entry'}},
+        {sys: {type: 'Asset'}},
+        {sys: {type: 'DeletedEntry'}},
+        {sys: {type: 'DeletedAsset'}}
+      ],
+      nextSyncToken: 'token'
+    })),
+    space: sinon.stub().returns(Promise.resolve({
+      locales: ['en-US']
+    }))
+  }
+
+  const managementClientMock = {
+    getSpace: sinon.stub().returns(Promise.resolve({
+      getContentTypes: sinon.stub().returns(Promise.resolve([
+        {sys: {type: 'ContentType'}}
+      ]))
+    }))
+  }
+
+  const preparedResponse = {
+    entries: [{sys: {type: 'Entry'}}],
+    assets: [{sys: {type: 'Asset'}}],
+    deletedEntries: [{sys: {type: 'DeletedEntry'}}],
+    deletedAssets: [{sys: {type: 'DeletedAsset'}}],
+    contentTypes: [{sys: {type: 'ContentType'}}],
+    locales: ['en-US'],
+    nextSyncToken: 'token',
+    isInitialSync: false
+  }
+
   getSourceSpace.__Rewire__('fs', fsMock)
+
+  return {
+    fsMock,
+    deliveryClientMock,
+    managementClientMock,
+    preparedResponse
+  }
 }
 
 function teardown () {
   getSourceSpace.__ResetDependency__('fs')
 }
 
-const deliveryClientMock = {
-  sync: sinon.stub().returns(Promise.resolve({
-    items: [
-      {sys: {type: 'Entry'}},
-      {sys: {type: 'Asset'}},
-      {sys: {type: 'DeletedEntry'}},
-      {sys: {type: 'DeletedAsset'}}
-    ],
-    nextSyncToken: 'token'
-  })),
-  space: sinon.stub().returns(Promise.resolve({
-    locales: ['en-US']
-  }))
-}
-
-const managementClientMock = {
-  getSpace: sinon.stub().returns(Promise.resolve({
-    getContentTypes: sinon.stub().returns(Promise.resolve([
-      {sys: {type: 'ContentType'}}
-    ]))
-  }))
-}
-
-const preparedResponse = {
-  entries: [{sys: {type: 'Entry'}}],
-  assets: [{sys: {type: 'Asset'}}],
-  deletedEntries: [{sys: {type: 'DeletedEntry'}}],
-  deletedAssets: [{sys: {type: 'DeletedAsset'}}],
-  contentTypes: [{sys: {type: 'ContentType'}}],
-  locales: ['en-US'],
-  nextSyncToken: 'token',
-  isInitialSync: false
-}
-
 test('Get source space with no file token', t => {
-  setup()
+  const {deliveryClientMock, managementClientMock, preparedResponse, fsMock} = setup()
   fsMock.readFileAsync.returns(Promise.reject('file not found'))
   getSourceSpace({
     deliveryClient: deliveryClientMock,
@@ -68,7 +75,7 @@ test('Get source space with no file token', t => {
 })
 
 test('Get source space with file token', t => {
-  setup()
+  const {deliveryClientMock, managementClientMock, preparedResponse, fsMock} = setup()
   fsMock.readFileAsync.withArgs('tokenfile').returns(Promise.resolve('newtoken'))
   getSourceSpace({
     deliveryClient: deliveryClientMock,
@@ -77,7 +84,7 @@ test('Get source space with file token', t => {
     nextSyncTokenFile: 'tokenfile'
   })
   .then(response => {
-    t.equals(deliveryClientMock.sync.secondCall.args[0].nextSyncToken, 'newtoken', 'syncs with provided token')
+    t.equals(deliveryClientMock.sync.firstCall.args[0].nextSyncToken, 'newtoken', 'syncs with provided token')
     t.deepLooseEqual(response, Object.assign({}, preparedResponse))
     teardown()
     t.end()
@@ -85,7 +92,7 @@ test('Get source space with file token', t => {
 })
 
 test('Get source space with forced sync from scratch', t => {
-  setup()
+  const {deliveryClientMock, managementClientMock, preparedResponse, fsMock} = setup()
   fsMock.readFileAsync.withArgs('tokenfile').returns(Promise.resolve('newtoken'))
   getSourceSpace({
     deliveryClient: deliveryClientMock,
