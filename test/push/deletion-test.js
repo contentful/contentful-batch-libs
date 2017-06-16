35c79e761c1f78255eb6543e20cb2f8a17e2f3c2
Benedikt Rötsch <benedikt@contentful.com>
2017-06-16 16:36:08 +0200

    feat(errors): replace error buffer and npmlog with log event emitters

--- test/push/deletion-test.js
+++ test/push/deletion-test.js
@@ -4,13 +4,29 @@ import Promise from 'bluebird'
 
 import {deleteEntities, __RewireAPI__ as deletionRewireAPI} from '../../lib/push/deletion'
 
-const logMock = {
-  info: sinon.stub()
+const fakeLogEmitter = {
+  emit: sinon.stub()
 }
-deletionRewireAPI.__Rewire__('log', logMock)
+
+const fakeErrorEmitter = {
+  emit: sinon.stub()
+}
+
+function setup () {
+  deletionRewireAPI.__Rewire__('logEmitter', fakeLogEmitter)
+  deletionRewireAPI.__Rewire__('errorEmitter', fakeErrorEmitter)
+}
+
+function teardown () {
+  fakeLogEmitter.emit.reset()
+  fakeErrorEmitter.emit.reset()
+  deletionRewireAPI.__ResetDependency__('logEmitter')
+  deletionRewireAPI.__ResetDependency__('errorEmitter')
+}
 
 test('Delete entities', (t) => {
-  logMock.info.reset()
+  setup()
+
   const entities = [
     { sys: {id: '123'}, delete: sinon.stub().returns(Promise.resolve()) },
     { sys: {id: '456'}, delete: sinon.stub().returns(Promise.resolve()) }
@@ -19,8 +35,30 @@ test('Delete entities', (t) => {
   .then((response) => {
     t.ok(entities[0].delete.called, 'delete asset 1')
     t.ok(entities[1].delete.called, 'delete asset 2')
-    t.equals(logMock.info.callCount, 2, 'logs deletion of two assets')
-    deletionRewireAPI.__ResetDependency__('log')
+    t.equals(fakeLogEmitter.emit.callCount, 2, 'logs deletion of two assets')
+    t.equals(fakeErrorEmitter.emit.callCount, 0, 'logs no errors')
+    teardown()
+    t.end()
+  })
+})
+
+test('Fails to delete one entity', (t) => {
+  setup()
+
+  const failedError = new Error('failed to delete')
+
+  const entities = [
+    { sys: {id: '123'}, delete: sinon.stub().returns(Promise.resolve()) },
+    { sys: {id: '456'}, delete: sinon.stub().returns(Promise.reject(failedError)) }
+  ]
+  deleteEntities(entities)
+  .then((response) => {
+    t.ok(entities[0].delete.called, 'delete asset 1')
+    t.ok(entities[1].delete.called, 'delete asset 2')
+    t.equals(fakeLogEmitter.emit.callCount, 1, 'logs deletion of one asset')
+    t.equals(fakeErrorEmitter.emit.callCount, 1, 'log one error')
+    t.equals(fakeErrorEmitter.emit.args[0][1], failedError, 'logs failed to delete error')
+    teardown()
     t.end()
   })
 })
