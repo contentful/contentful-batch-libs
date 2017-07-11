bfba4c43fad702fc280a47553b12a543ec4008e6
Benedikt Rötsch <benedikt@contentful.com>
2017-07-11 09:58:00 +0200

    style(eslint): upgrade eslint and fix indentations based on new rules

--- test/push/creation-test.js
+++ test/push/creation-test.js
@@ -29,15 +29,15 @@ test('Create entities', (t) => {
   ], [
     {sys: {id: '123', version: 6}, update: updateStub}
   ])
-  .then((response) => {
-    t.equals(space.createAssetWithId.callCount, 1, 'creates one missing asset')
-    t.equals(updateStub.callCount, 1, 'updates one existing assets')
-    t.equals(fakeLogEmitter.emit.callCount, 2, 'logs creation of two assets')
-    const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
-    t.notOk(logLevels.includes('error'), 0, 'logs no errors')
-    teardown()
-    t.end()
-  })
+    .then((response) => {
+      t.equals(space.createAssetWithId.callCount, 1, 'creates one missing asset')
+      t.equals(updateStub.callCount, 1, 'updates one existing assets')
+      t.equals(fakeLogEmitter.emit.callCount, 2, 'logs creation of two assets')
+      const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
+      t.notOk(logLevels.includes('error'), 0, 'logs no errors')
+      teardown()
+      t.end()
+    })
 })
 
 test('Create entries', (t) => {
@@ -56,16 +56,16 @@ test('Create entries', (t) => {
     {sys: {id: '123', version: 6}, update: updateStub}
   ]
   createEntries({space: space, skipContentModel: false}, entries, destinationEntries)
-  .then((response) => {
-    t.equals(space.createEntryWithId.callCount, 1, 'create entries with the same id')
-    t.equals(space.createEntry.callCount, 1, 'create entries even when the id is not provided')
-    t.equals(updateStub.callCount, 1, 'update entries')
-    t.equals(fakeLogEmitter.emit.callCount, 3, 'logs creation/update of three entries')
-    const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
-    t.notOk(logLevels.includes('error'), 0, 'logs no errors')
-    teardown()
-    t.end()
-  })
+    .then((response) => {
+      t.equals(space.createEntryWithId.callCount, 1, 'create entries with the same id')
+      t.equals(space.createEntry.callCount, 1, 'create entries even when the id is not provided')
+      t.equals(updateStub.callCount, 1, 'update entries')
+      t.equals(fakeLogEmitter.emit.callCount, 3, 'logs creation/update of three entries')
+      const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
+      t.notOk(logLevels.includes('error'), 0, 'logs no errors')
+      teardown()
+      t.end()
+    })
 })
 
 test('Create entries and remove unknown fields', (t) => {
@@ -96,16 +96,16 @@ test('Create entries and remove unknown fields', (t) => {
   ]
 
   createEntries({space: {}, skipContentModel: true}, entries, destinationEntries)
-  .then((response) => {
-    t.equals(updateStub.callCount, 2, 'update entries')
-    t.ok('existingfield' in entries[0].transformed.fields, 'keeps known field')
-    t.notOk('gonefield' in entries[0].transformed.fields, 'removes unknown field')
-    t.equals(fakeLogEmitter.emit.callCount, 1, 'logs creation of one entry')
-    const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
-    t.notOk(logLevels.includes('error'), 0, 'logs no errors')
-    teardown()
-    t.end()
-  })
+    .then((response) => {
+      t.equals(updateStub.callCount, 2, 'update entries')
+      t.ok('existingfield' in entries[0].transformed.fields, 'keeps known field')
+      t.notOk('gonefield' in entries[0].transformed.fields, 'removes unknown field')
+      t.equals(fakeLogEmitter.emit.callCount, 1, 'logs creation of one entry')
+      const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
+      t.notOk(logLevels.includes('error'), 0, 'logs no errors')
+      teardown()
+      t.end()
+    })
 })
 
 test('Fails to create locale if it already exists', (t) => {
@@ -123,11 +123,11 @@ test('Fails to create locale if it already exists', (t) => {
   space.createLocale.returns(Promise.reject(errorValidationFailed))
   const entity = { original: { sys: {} }, transformed: { sys: {} } }
   createEntities({space: space, type: 'Locale'}, [entity], [{sys: {}}])
-  .then((entities) => {
-    t.equals(entities[0], entity)
-    const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
-    t.notOk(logLevels.includes('error'), 0, 'logs no errors')
-    t.end()
-    teardown()
-  })
+    .then((entities) => {
+      t.equals(entities[0], entity)
+      const logLevels = fakeLogEmitter.emit.args.map((args) => args[0])
+      t.notOk(logLevels.includes('error'), 0, 'logs no errors')
+      t.end()
+      teardown()
+    })
 })
