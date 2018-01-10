9eec7ffbcefd45a19932473cd5188bc863d2d607
Benedikt Rötsch <benedikt@contentful.com>
2018-01-10 15:10:38 +0100

    test(unit): add tests for create clients and get entity name

---
+++ test/utils/get-entity-name.test.js
@@ -0,0 +1,82 @@
+import getEntityName from '../../lib/utils/get-entity-name'
+
+test('get name by name property', () => {
+  const name = getEntityName({
+    name: 'entityName'
+  })
+  expect(name).toBe('entityName')
+})
+
+test('get name by field called title', () => {
+  const name = getEntityName({
+    fields: {
+      title: {
+        'en-US': 'entityName'
+      }
+    }
+  })
+  expect(name).toBe('entityName')
+})
+
+test('get name by field called name', () => {
+  const name = getEntityName({
+    fields: {
+      name: {
+        'en-US': 'entityName'
+      }
+    }
+  })
+  expect(name).toBe('entityName')
+})
+
+test('get name by name property and attaches id', () => {
+  const name = getEntityName({
+    sys: {
+      id: 'entityId'
+    },
+    name: 'entityName'
+  })
+  expect(name).toBe('entityName (entityId)')
+})
+
+test('get name by field called title and attaches id', () => {
+  const name = getEntityName({
+    sys: {
+      id: 'entityId'
+    },
+    fields: {
+      title: {
+        'en-US': 'entityName'
+      }
+    }
+  })
+  expect(name).toBe('entityName (entityId)')
+})
+
+test('get name by field called name and attaches id', () => {
+  const name = getEntityName({
+    sys: {
+      id: 'entityId'
+    },
+    fields: {
+      name: {
+        'en-US': 'entityName'
+      }
+    }
+  })
+  expect(name).toBe('entityName (entityId)')
+})
+
+test('fall back to id when no name is found', () => {
+  const name = getEntityName({
+    sys: {
+      id: 'entityId'
+    }
+  })
+  expect(name).toBe('entityId')
+})
+
+test('returns unknown when not even an ID is present', () => {
+  const name = getEntityName({})
+  expect(name).toBe('unknown')
+})
