6e5e5d0c8d89d86adf4f8be42f1cd0136f796ab2
Brad Turner <dev.bradturner@gmail.com>
2023-10-24 18:39:55 +1100

    Minor tweaks to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\Brad\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "",
 
   // Automatically clear mock calls, instances, contexts and results before every test
   // clearMocks: false,
@@ -28,7 +28,7 @@ const config: Config = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['\\\\node_modules\\\\', '\\\\vendor\\\\'],
+  coveragePathIgnorePatterns: ['node_modules', 'vendor'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
