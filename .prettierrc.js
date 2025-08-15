8e30046e1f371972b6e790d9dab8c671a7e2b03e
Benedikt Rötsch <benedikt@hashbite.net>
2025-08-15 11:51:36 +0200

    chore: add editor and prettier config that match our coding style to avoid annoying issues when using advanced code editors

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+}
